c583eedb96c24296c9bc7f90bd0d566e424fb870
Poulami <catchpolo@gmail.com>
2023-03-07 01:31:59 +0100

    including dipole for eeg

--- model/cfg.py
+++ model/cfg.py
@@ -60,8 +60,8 @@ cfg.recordLFP = [[x, 1000, 100] for x in range(100, 2200, 200)]
 #cfg.saveLFPPops =  cfg.allCorticalPops #, "IT3", "SOM3", "PV3", "VIP3", "NGF3", "ITP4", "ITS4", "IT5A", "CT5A", "IT5B", "PT5B", "CT5B", "IT6", "CT6"]
 
 cfg.recordDipole = True
-# cfg.saveDipoleCells = ['all']
-# cfg.saveDipolePops = cfg.allpops
+cfg.saveDipoleCells = ['all']
+cfg.saveDipolePops = cfg.allpops
 
 
 #------------------------------------------------------------------------------
