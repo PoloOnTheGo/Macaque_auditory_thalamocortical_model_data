712a03959f8e4afefadaedf760c4887ddb34f92f
Poulami <catchpolo@gmail.com>
2023-03-19 21:55:19 +0100

    reverting cfg changes

--- model/batch.py
+++ model/batch.py
@@ -294,7 +294,7 @@ def custom_speech(filename):
     # initCfg['ICThalInput'] = {'file': 'data/ICoutput/ICoutput_CF_9600_10400_wav_01_ba_peter.mat',
                             'startTime': 1,
                             'weightE': 1,#1.0,
-                            'weightI': 0.8,#1.0,
+                            'weightI': 0.5,#1.0,
                             'probE': 0.12,
                             'probI': 0.25, #0.25
                             'seed': 1}  
