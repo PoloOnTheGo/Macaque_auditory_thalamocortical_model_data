edac0ccd7a0b818b10d8175379995d884e848634
Poulami <catchpolo@gmail.com>
2023-03-20 03:12:42 +0100

    changes from 'weightE': 1.0 to 'weightE': 1.5

--- model/batch.py
+++ model/batch.py
@@ -293,8 +293,8 @@ def custom_speech(filename):
     initCfg['ICThalInput'] = {'file': '../data/ICoutput/ASSR_25ms_40Hz_click_train_10sec.mat',
     # initCfg['ICThalInput'] = {'file': 'data/ICoutput/ICoutput_CF_9600_10400_wav_01_ba_peter.mat',
                             'startTime': 1,
-                            'weightE': 1,#1.0,
-                            'weightI': 0.2,#1.0,
+                            'weightE': 1.5,#1.0,
+                            'weightI': 0.5,#1.0,
                             'probE': 0.12,
                             'probI': 0.25, #0.25
                             'seed': 1}  
