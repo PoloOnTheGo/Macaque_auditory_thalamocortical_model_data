f65b0c2fb92024e93c30c9eef1e19c5bba66fad1
Poulami <catchpolo@gmail.com>
2023-06-04 23:32:52 +0200

    params[('ICThalInput', 'probE')] = [0.12, 0.26, 0.38, 0.5] params[('ICThalInput', 'file')] = ['../data/ICoutput/20Hz_1000BMF_4sec.mat', '../data/ICoutput/30Hz_1000BMF_4sec.mat', '../data/ICoutput/40Hz_1000BMF_4sec.mat'], (100 *20 = 2000) cells, 'startTime': 2600, 'weightE': 0.7,#1.0,'weightI': 0.6,#1.0, 'probI': 0.8, #0.25 7000ms, TU 1 node 32 core

--- model/batch.py
+++ model/batch.py
@@ -266,12 +266,12 @@ def custom_speech(filename):
     # params[('seeds', 'conn')] = [4321+(17*i) for i in range(5)]
     # params[('seeds', 'stim')] = [1234+(17*i) for i in range(5)]
 
-    # params[('ICThalInput', 'probE')] = [0.26, 0.38, 0.5]
+    params[('ICThalInput', 'probE')] = [0.12, 0.26, 0.38, 0.5]
     # params[('ICThalInput', 'probI')] = [0.3, 0.5, 0.8]
     # params[('ICThalInput', 'weightE')] = [0.6, 0.7, 0.8]
     # params[('ICThalInput', 'weightI')] = [0.5, 0.6, 0.7]
 
-    # params[('ICThalInput', 'file')] = ['../data/ICoutput/20Hz_40BMF_4sec.mat', '../data/ICoutput/30Hz_40BMF_4sec.mat']
+    params[('ICThalInput', 'file')] = ['../data/ICoutput/20Hz_1000BMF_4sec.mat', '../data/ICoutput/30Hz_1000BMF_4sec.mat', '../data/ICoutput/40Hz_1000BMF_4sec.mat']
 
     groupedParams = []
 
@@ -292,11 +292,11 @@ def custom_speech(filename):
     #initCfg[('analysis', 'plotCSD', 'timeRange')] = [1500, 1700]
 
     initCfg['ICThalInput'] = {
-                            'file': '../data/ICoutput/40Hz_1000BMF_4sec.mat',
+                            # 'file': '../data/ICoutput/20Hz_1000BMF_4sec.mat',
                             'startTime': 2600,
                             'weightE': 0.7,#1.0,
                             'weightI': 0.6,#1.0,
-                            'probE': 0.26,
+                            # 'probE': 0.26,
                             'probI': 0.8, #0.25
                             'seed': 1}  
 
